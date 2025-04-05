e040d53dcdad2dfcf035ff70e4ff585a02d31d00
self1027 <diasmurilo02@gmail.com>
2025-04-04 22:28:50 -0300

    dictionary created

---
+++ constants/denunciationSenders.js
@@ -0,0 +1,5 @@
+export const DENUNCIATION_SENDER ={
+    PESSOA_PRESENCIAL: "Denuncia Anônima Realizada Presencialmente",
+    PESSOA_TELEFONE: "Denuncia Anônima Realizada via Telefone",
+    OFICIO: "Ofício Recebido"
+}
