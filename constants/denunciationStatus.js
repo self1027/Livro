a98d44c2686062f51524ef117d1609b67fa9051c
self1027 <diasmurilo02@gmail.com>
2025-04-10 12:27:30 -0300

    added a slug property to the dictionary

--- constants/denunciationStatus.js
+++ constants/denunciationStatus.js
@@ -1,34 +1,32 @@
 module.exports = DENUNCIATION_STATUS = {
     REGISTRADA: {
         label: "Registrada",
-        color: "#3498db" // Azul
+        color: "#3498db", // Azul
+        slug: "REGISTRADA"
     },
     EM_ANDAMENTO: {
         label: "Em Andamento",
-        color: "#f39c12" // Laranja
+        color: "#f39c12", // Laranja
+        slug: "EM_ANDAMENTO"
     },
     NOTIFICADA: {
         label: "Notificada",
-        color: "#2ecc71" // Verde
-    },
-    NAO_PROCEDE: {
-        label: "Não Procede",
-        color: "#e74c3c" // Vermelho
+        color: "#2ecc71", // Verde
+        slug: "NOTIFICADA"
     },
     EM_PUBLICACAO: {
         label: "Em Publicação",
-        color: "#8e44ad" // Roxo
-    },
-    ENCAMINHADA_OUTRO_SETOR: {
-        label: "Encaminhada para Outro Setor",
-        color: "#f1c40f" // Amarelo
+        color: "#8e44ad", // Roxo
+        slug: "EM_PUBLICACAO"
     },
     FEITO_AIF: {
         label: "Em Processo Administrativo",
-        color: "#16a085" // Verde escuro
+        color: "#16a085", // Verde escuro
+        slug: "FEITO_AIF"
     },
     FINALIZADA: {
         label: "Finalizada",
-        color: "#95a5a6" // Cinza
+        color: "#95a5a6", // Cinza
+        slug: "FINALIZADA"
     }
 };
