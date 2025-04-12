1b7b10597c905e5fffc0cf250b90542f2acb4bc1
self1027 <diasmurilo02@gmail.com>
2025-04-11 22:12:11 -0300

    added active type to user

--- users/usersModel.js
+++ users/usersModel.js
@@ -1,12 +1,19 @@
 const sequelize = require('sequelize');
-const connection = require('../database/database.js');
+const connection = require('../database/database.js');  
 
 const user = connection.define('users', {
     name: {
         type: sequelize.STRING,
         allowNull: false
+    },
+    ativo: {
+        type: sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
     }
 });
 
+
 user.sync();
+
 module.exports = user;
