9eff92ec65db3150288e3081821b7b772259198b
Murilo D. <129670228+self1027@users.noreply.github.com>
2025-06-05 19:46:26 -0300

    Update pagination.js to commomJS

--- middlewares/pagination.js
+++ middlewares/pagination.js
@@ -1,6 +1,6 @@
 const DEFAULT_LIMIT = 50;
 
-export default (req, res, next) => {
+module.exports = (req, res, next) => {
     const offset = parseInt(req.query.offset) || 0;
     const limit = parseInt(req.query.limit) || DEFAULT_LIMIT;
   
