c728107374b73333090d4535bb9cc09f01b1fbe9
self1027 <diasmurilo02@gmail.com>
2026-03-25 13:20:32 +0000

    bug fix: minor changes

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ const __dirname = path.dirname(__filename)
 app.set('views', path.join(__dirname, '../views'))
 app.set('view engine', 'ejs')
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, '../public')))
 
 // Body Parser
 app.use(bodyParser.urlencoded({
